424a1d004e2d5cf0eb336b700a5143212a2eca79
ksyfnn <syifankasyifan@gmail.com>
2022-09-05 11:52:02 +0700

    menambahkan koneksi

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// parser application /json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.listen(3000, () => {
+    console.log(`server started on port`);
+});
