b1cce8caeabb0d774f13b101765a9c6bdc98107c
ksyfnn <syifankasyifan@gmail.com>
2022-09-05 13:31:13 +0700

    setting router di server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 // parser application /json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// memanggil routes
+var route = require('./router')
+routes(app);
+
 app.listen(3000, () => {
     console.log(`server started on port`);
 });
